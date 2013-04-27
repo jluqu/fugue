3fe9701489daed3224b3f8b610e3890ebb9ece1a
Joe Luquette <jtrix6@gmail.com>
2013-04-27 19:41:47 -0400

    partial implementation of LevelXmlProcessor

--- LevelXmlParser.cpp
+++ LevelXmlParser.cpp
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <vector>
 #include <libxml/parser.h>
 #include <libxml/tree.h>
 
 #include "LevelXmlParser.h"
+#include "ObjectFactory.h"
+#include "LevelObject.h"
 
 LevelXmlParser* LevelXmlParser::m_pInstance = NULL;
 
@@ -21,17 +24,45 @@ LevelXmlParser::LevelXmlParser()
 
 LevelXmlParser::~LevelXmlParser()
 {
-	delete m_pInstance;
+    delete m_pInstance;
 }
 
-bool LevelXmlParser::load(const char* filename)
+bool LevelXmlParser::load(const char* filename, std::vector<LevelObject*> objList)
 {
-	
     xmlDocPtr doc = xmlReadFile(filename, NULL, 0);
     if (doc == NULL) {
-		printf("Failed to parse %s\n", filename);
-		return false;
-	}
-	xmlFreeDoc(doc);
-	return true;
+        printf("Failed to parse %s\n", filename);
+        return false;
+    }
+    xmlNodePtr root = xmlDocGetRootElement(doc);
+    if (root == NULL) {
+        printf("Can't find root node in the level file\n");
+        return false;
+
+    } else if (xmlStrcmp(root->name, (const xmlChar *)"level")) { 
+        printf("Root node is called %s, expected 'root'\n", root->name);
+        return false;
+    }
+    processLevel(root, objList);
+
+    xmlFreeDoc(doc);
+    return true;
+}
+
+void LevelXmlParser::processLevel(xmlNodePtr cur, std::vector<LevelObject*> objList)
+{
+    xmlNodePtr child = cur->xmlChildrenNode;
+    while (child != NULL)
+    {
+        if (!xmlStrcmp(cur->name, (const xmlChar *)"block"))
+        {
+            processBlock(child, objList);
+        }
+        child = child->next;
+    }
+}
+
+void LevelXmlParser::processBlock(xmlNodePtr cur, std::vector<LevelObject*> objList)
+{
+    printf("Found a block\n");
 }
--- LevelXmlParser.h
+++ LevelXmlParser.h
@@ -1,18 +1,27 @@
 #ifndef _LEVEL_XML_PARSER_H_
 #define _LEVEL_XML_PARSER_H_
 
+//TODO: which of these do we need?
+#include <libxml/parser.h>
+#include <libxml/tree.h>
+#include <vector>
+
+#include "LevelObject.h"
+
 class LevelXmlParser
 {
     public:
-		// I'm a singleton!
         static LevelXmlParser* getInstance();
         
-        bool load(const char* filename);
+        bool load(const char* filename, std::vector<LevelObject*> objList);
         
     private:
         LevelXmlParser();
         ~LevelXmlParser();
     	static LevelXmlParser* m_pInstance;
+    	
+    	void processLevel(xmlNodePtr cur, std::vector<LevelObject*> objList);
+    	void processBlock(xmlNodePtr cur, std::vector<LevelObject*> objList);
 };
 
 #endif
